439e592270a49a3ec6c623e8a46d03f913bd6253
Edwin Leonardo Quintero Castaño <equinteroc@choucairtesting.com>
2022-03-24 23:19:19 -0500

    login y clave ok

--- src/test/java/com/choucair/reto/saucedemo/pageobjets/SaucedemoLoginPage.java
+++ src/test/java/com/choucair/reto/saucedemo/pageobjets/SaucedemoLoginPage.java
@@ -1,14 +1,30 @@
 package com.choucair.reto.saucedemo.pageobjets;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 
 @DefaultUrl("https://www.saucedemo.com/")
 
 public class SaucedemoLoginPage extends PageObject {
+    // Campo usuario
+    @FindBy(id = "user-name")
+    public WebElementFacade txtUsername;
+
+    // Campo password
+    @FindBy(id = "password")
+    public WebElementFacade txtPassword;
+
+    // Boton login
+    @FindBy(id = "login-button")
+    public WebElementFacade btnLogin;
 
 
     public void ingresarDatos(String strUsuario, String strPass) {
+        txtUsername.sendKeys(strUsuario);
+        txtPassword.sendKeys(strPass);
+        btnLogin.click();
     }
 
     public void verificaHome() {
